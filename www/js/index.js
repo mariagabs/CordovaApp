af1d6ad807f918fc7f93d1e788750f390cf5312f
Maria Gabriela <mgmakamine@gmail.com>
2021-09-20 01:02:02 -0300

    Correção demonstração da descrição dos cards tab3

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,12 @@
-window.onload = function() {
-    AppController.init();
+window.onload = function () {
+  AppController.init();
 };
 
-function hideShowDiv() {
-    var x = document.getElementById("card-details");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+function hideShowDiv(index) {
+  var x = document.getElementById(index);
+  if (x.style.display === "none" || x.style.display === "") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
